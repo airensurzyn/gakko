f74dce149eea355639175a1383544267b5b6eea6
Airen <nyzrus@gmail.com>
2020-12-15 18:19:27 -0800

    Add testing for new route in courses

--- course/src/routes/__test__/new.test.ts
+++ course/src/routes/__test__/new.test.ts
@@ -10,3 +10,100 @@ it('has a route handler listening to /api/tickets for post requests', async () =
 
     expect(response.status).not.toEqual(404);
 });
+
+it('authenticates user requests', async () => {
+    const response = await request(app)
+        .post('/api/courses')
+        .send({});
+    expect(response.status).toEqual(401);
+});
+
+it('does not return a 401 if user is authenticated', async () => {
+    const response = await request(app)
+        .post('/api/courses')
+        .set('Cookie', global.signup())
+        .send({
+            title:"Test Course",
+            description: "It's about testing things",
+            languageTopic:"japanese",
+            instructionLanguage:"english",
+            status: "closed",
+            price: 5,
+            headerImage: "http://someurl.com",
+        });
+    expect
+});
+
+it('returns an error if an invalid price is provided', async () => {
+    const response = await request(app)
+        .post('/api/courses')
+        .set('Cookie', global.signup())
+        .send({
+            title:"Test Course",
+            description: "It's about testing things",
+            languageTopic:"japanese",
+            instructionLanguage:"english",
+            status: "closed",
+            price: -5,
+            headerImage: "http://someurl.com",
+        }).expect(400);
+});
+
+it('returns an error if no title is provided', async () => {
+    const response = await request(app)
+        .post('/api/courses')
+        .set('Cookie', global.signup())
+        .send({
+            
+            description: "It's about testing things",
+            languageTopic:"japanese",
+            instructionLanguage:"english",
+            status: "closed",
+            price: -5,
+            headerImage: "http://someurl.com",
+        }).expect(400);
+});
+
+it('creates a valid course with valid inputs', async () => {
+    let courses = await Course.find({});
+    expect(courses.length).toEqual(0);
+
+    await request(app)
+        .post('/api/courses')
+        .set('Cookie', global.signup())
+        .send({
+            title:"Test Course",
+            description: "It's about testing things",
+            languageTopic:"japanese",
+            instructionLanguage:"english",
+            status: "closed",
+            price: 5,
+            headerImage: "http://someurl.com",
+        }).expect(201);
+    
+    courses = await Course.find({});
+    expect(courses.length).toEqual(1);
+    expect(courses[0].title).toEqual("Test Course");
+    expect(courses[0].description).toEqual("It's about testing things");
+    expect(courses[0].languageTopic).toEqual("japanese");
+    expect(courses[0].instructionLanguage).toEqual("english");
+    expect(courses[0].price).toEqual(5);
+});
+
+it('publishes the course create event', async () => {
+    await request(app)
+        .post('/api/courses')
+        .set('Cookie', global.signup())
+        .send({
+            title:"Test Course",
+            description: "It's about testing things",
+            languageTopic:"japanese",
+            instructionLanguage:"english",
+            status: "closed",
+            price: 5,
+            headerImage: "http://someurl.com",
+        })
+        .expect(201);
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+})
--- course/src/routes/new.ts
+++ course/src/routes/new.ts
@@ -10,7 +10,8 @@ const router = express.Router();
 router.post('/api/courses', requireAuth, [body('title').not().isEmpty().withMessage('Title is required'),
     body('description').not().isEmpty().withMessage('Description is required'),
     body('languageTopic').not().isEmpty().withMessage('Language Topic is required'),
-    body('instructionLanguage').not().isEmpty().withMessage('Instruction Language is required')],
+    body('instructionLanguage').not().isEmpty().withMessage('Instruction Language is required'),
+    body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0')],
     validateRequest, async (req: Request, res: Response) => {
         
         const { title, description, languageTopic, instructionLanguage, headerImage, price } = req.body;
