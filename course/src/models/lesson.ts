4b4654c3add942ed1cc336b369d8df5b770ae93c
Airen <nyzrus@gmail.com>
2020-12-20 17:54:58 -0800

    Lesson model

---
+++ course/src/models/lesson.ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+
+interface LessonDoc extends mongoose.Document {
+    title: string;
+    videoUrl: string;
+    moduleId: string;
+    version: number;
+}
+
+interface LessonModel extends mongoose.Model<LessonDoc> {
+    build(attrs: LessonAttributes): LessonDoc;
+}
+
+interface LessonAttributes {
+    title: string;
+    videoUrl: string;
+    moduleId: string;
+}
+
+const lessonSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    videoUrl:{
+        type: String,
+        required: true
+    },
+    moduleId: {
+        type: String,
+        required: true
+    }
+}, {
+    toJSON:{
+        transform(doc, ret) {
+            ret.id = ret._id;
+            delete ret._id;
+        }
+    }
+});
+
+lessonSchema.set('versionKey', 'version');
+lessonSchema.plugin(updateIfCurrentPlugin);
+
+lessonSchema.statics.build = (attrs: LessonAttributes) => {
+    return new lessonSchema(attrs);
+}
+
+const Lesson = mongoose.model<LessonDoc, LessonModel>('Lesson', lessonSchema);
+
+export { Lesson };
